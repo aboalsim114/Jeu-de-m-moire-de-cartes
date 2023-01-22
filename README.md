637e3c7d701ddd9dd79d88fe9883ef93f74209fd
aboalsim114 <sami.abdulhalim@hetic.net>
2023-01-22 17:03:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Jeu de mémoire de cartes
+
+Ce projet est un jeu de mémoire de cartes créé à l'aide de JavaScript. Il permet aux utilisateurs de cliquer sur des cartes pour les retourner et essayer de trouver des correspondances.
+
+
+## Comment jouer
+
+
+
+```bash
+  1. Ouvrir le fichier HTML dans un navigateur
+  2. Cliquez sur les cartes pour les retourner
+  3. Essayez de trouver des cartes avec des nombres aléatoires correspondants
+  4. Utilisez les boutons de rejouer pour relancer la partie.
+
+```
+
+
+
+    
+## Règles du jeu
+
+ - Chaque carte a un nombre aléatoire attribué à l'aide d'une fonction aléatoire
+ - Les utilisateurs peuvent retourner deux cartes à la fois
+ - Si les cartes correspondent, elles sont désactivées en changeant leur couleur en vert
+ - Si les cartes ne correspondent pas, elles sont remises en place après un délai de 1 seconde
+ - Il y a un compteur d'essaie pour limiter les tentatives de l'utilisateur
+ - Si l'utilisateur gagne ou épuise tous ses essais, un message s'affiche pour informer l'utilisateur et il ne peut plus jouer
+ 
+
+
+## Authors
+
+- [@sami abdulhalim ](https://www.linkedin.com/in/sami-abdulhalim/)
+
---
+++ js/app.js
@@ -0,0 +1,122 @@
+// Création d'un tableau pour stocker les cartes retournées
+let flippedCards = [];
+const replayButton = document.getElementById('replay-button');
+const nb_essaie = document.getElementById('nb_essaie');
+// Sélection de toutes les cartes sur la page
+const cards = document.querySelectorAll('.memory-card');
+
+
+
+// Ajout d'un événement de clic à chaque carte
+cards.forEach(card => {
+    // Génération d'un nombre aléatoire 
+    const randomNum = Math.floor(Math.random() * cards.length);
+    card.innerHTML = `<span style="font-size : 30px" class="random-num">${randomNum}</span>`;
+    card.querySelector('.random-num').style.display = "none";
+    card.addEventListener('click', flipCard);
+});
+
+// Fonction pour retourner une carte
+function flipCard() {
+
+    // Vérification si la carte est déjà retournée ou si deux cartes sont déjà retournées
+
+    if (this.classList.contains('flip') || flippedCards.length === 2) return;
+
+    // Ajout de la classe "flip" à la carte
+    this.classList.add('flip');
+    this.querySelector('.random-num').style.display = "block";
+
+    // Ajout de la carte au tableau des cartes retournées
+    flippedCards.push(this);
+
+    // Vérification si deux cartes sont retournées
+    if (flippedCards.length === 2) {
+        // Vérification si les cartes correspondent
+        checkForMatch();
+    }
+}
+
+
+let matches = 0;
+let essaie = 5;
+
+nb_essaie.innerHTML = "nombres d'essaie = " + essaie;
+
+function checkEssaie() {
+    if (essaie <= 0) {
+        Swal.fire(
+            'Fin de la partie',
+            'Vous avez épuisé tous vos essais',
+            'error'
+        )
+        cards.forEach(card => {
+            card.removeEventListener('click', flipCard);
+        });
+        return;
+    }
+}
+
+
+
+
+// Fonction pour vérifier si les cartes correspondent
+function checkForMatch() {
+    // Récupération du contenu HTML de la balise span contenant le nombre aléatoire des deux cartes retournées
+    const card1 = flippedCards[0].querySelector('.random-num').textContent;
+    const card2 = flippedCards[1].querySelector('.random-num').textContent;
+
+    // Vérification si les cartes correspondent
+    if (card1 === card2) {
+        // Si elles correspondent, on désactive les cartes en changeant leur couleur en vert
+        flippedCards[0].style.backgroundColor = "#42ba96";
+        flippedCards[1].style.backgroundColor = "#42ba96";
+        disableCards();
+
+        matches++;
+
+        if (matches === 3) {
+            console.log("vous avez gagné ");
+            Swal.fire(
+                'Bravo',
+                'Vous avez gagné la partie',
+                'success'
+            )
+            cards.forEach(card => {
+                card.removeEventListener('click', flipCard);
+            });
+            replayButton.style.display = 'block';
+
+
+        }
+    } else {
+        // Si elles ne correspondent pas, on les remet en place après un délai de 1 seconde
+        setTimeout(unflipCards, 1000);
+
+        essaie -= 1;
+        nb_essaie.innerHTML = "nombres d'essaie = " + essaie;
+
+        checkEssaie();
+    }
+}
+
+
+replayButton.addEventListener('click', function() {
+    window.location.reload();
+});
+
+// Disable cards function
+function disableCards() {
+    flippedCards[0].removeEventListener('click', flipCard);
+    flippedCards[1].removeEventListener('click', flipCard);
+    flippedCards = [];
+}
+
+// Unflip cards function
+function unflipCards() {
+    flippedCards[0].classList.remove('flip');
+    flippedCards[1].classList.remove('flip');
+    flippedCards[0].querySelector('.random-num').style.display = "none";
+    flippedCards[1].querySelector('.random-num').style.display = "none";
+    flippedCards = [];
+}
